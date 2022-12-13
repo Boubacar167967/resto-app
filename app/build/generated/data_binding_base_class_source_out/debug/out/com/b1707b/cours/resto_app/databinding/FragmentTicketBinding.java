b7a8411e7f4fb81e222f87e4adba1b8cbce700bb
boubacar167967 <boubacarcidi77@gmail.com>
2022-12-13 20:31:12 +0000

    achat de tickets

--- app/build/generated/data_binding_base_class_source_out/debug/out/com/b1707b/cours/resto_app/databinding/FragmentTicketBinding.java
+++ app/build/generated/data_binding_base_class_source_out/debug/out/com/b1707b/cours/resto_app/databinding/FragmentTicketBinding.java
@@ -4,25 +4,33 @@ package com.b1707b.cours.resto_app.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.viewbinding.ViewBinding;
+import androidx.viewbinding.ViewBindings;
 import com.b1707b.cours.resto_app.R;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 
 public final class FragmentTicketBinding implements ViewBinding {
   @NonNull
-  private final FrameLayout rootView;
+  private final RelativeLayout rootView;
 
-  private FragmentTicketBinding(@NonNull FrameLayout rootView) {
+  @NonNull
+  public final Button fragmentTicketBtnLaunch;
+
+  private FragmentTicketBinding(@NonNull RelativeLayout rootView,
+      @NonNull Button fragmentTicketBtnLaunch) {
     this.rootView = rootView;
+    this.fragmentTicketBtnLaunch = fragmentTicketBtnLaunch;
   }
 
   @Override
   @NonNull
-  public FrameLayout getRoot() {
+  public RelativeLayout getRoot() {
     return rootView;
   }
 
@@ -43,10 +51,19 @@ public final class FragmentTicketBinding implements ViewBinding {
 
   @NonNull
   public static FragmentTicketBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.fragment_ticket_btnLaunch;
+      Button fragmentTicketBtnLaunch = ViewBindings.findChildViewById(rootView, id);
+      if (fragmentTicketBtnLaunch == null) {
+        break missingId;
+      }
 
-    return new FragmentTicketBinding((FrameLayout) rootView);
+      return new FragmentTicketBinding((RelativeLayout) rootView, fragmentTicketBtnLaunch);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
--- app/src/main/java/com/b1707b/cours/resto_app/TicketFragment.java
+++ app/src/main/java/com/b1707b/cours/resto_app/TicketFragment.java
@@ -2,11 +2,20 @@ package com.b1707b.cours.resto_app;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.HashMap;
+
+import sdk.paytech.sn.PCallback;
+import sdk.paytech.sn.PayTech;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,7 +23,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class TicketFragment extends Fragment {
-
+    private Button mButton;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -58,7 +67,32 @@ public class TicketFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ticket, container, false);
+        View view = inflater.inflate(R.layout.fragment_ticket, container, false);
+        mButton = view.findViewById(R.id.fragment_ticket_btnLaunch);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("click", "click on button");
+                HashMap params = new HashMap<>();
+                params.put("item_id", 2);
+
+                new PayTech((AppCompatActivity) getActivity()).setRequestTokenUrl("https://sample.paytech.sn/paiement.php").setParams(params).setPresentationMode(PayTech.FLOATING_VIEW) // optional default to FULL_SCREEN
+                        .setFloatTopMargin(25) // optional default to 25
+                        .setLoadingDialogText("Chargement") //optional Chargement
+                        .setCallback(new PCallback() {
+                            @Override
+                            public void onResult(Result result) {
+                                if (result == Result.SUCCESS) {
+                                    Toast.makeText(getActivity(), "Paiement Effectuer", Toast.LENGTH_SHORT).show();
+                                } else if (result == Result.CANCEL) {
+                                    Toast.makeText(getActivity(), "Vous avez annulez le paiement", Toast.LENGTH_SHORT).show();
+                                } else if (result == Result.ERROR) {
+                                    Toast.makeText(getActivity(), "Erreur lors du paiement", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }).send();
+            }
+        });
+        return view;
     }
 }
